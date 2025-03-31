0c40b5aa9ffe1510093e9b78e99e995f7fdaf5c0
Carter Sifferman <cpsiff@gmail.com>
2025-03-31 10:43:50 -0500

    change paper project page paths for backwards compatability

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,30 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/geometric-calibration',
+        destination: '/geometric-calibration/index.html',
+        permanent: true,
+      },
+      {
+        source: '/towards_3d_vision',
+        destination: '/towards_3d_vision/index.html',
+        permanent: true,
+      },
+      {
+        source: '/unlocking_proximity_sensors',
+        destination: '/unlocking_proximity_sensors/index.html',
+        permanent: true,
+      },
+      {
+        source: '/using_a_distance_sensor',
+        destination: '/using_a_distance_sensor/index.html',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
